35bcad753ea3822899fde4dc7b39c3a8f27a80ef
createjin <createsejin@gmail.com>
2024-03-10 14:21:12 +0900

    2024-03-10-000

--- src/study/study05.rs
+++ src/study/study05.rs
@@ -50,3 +50,35 @@ pub fn study001() {
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
+
+#[allow(dead_code)]
+fn main_000() {
+    println!("Hello, world!");
+    let s1 = String::from("hello");
+    let len = calculate_length(&s1);
+    println!("The length of '{}' is {}.", s1, len);
+} 
+#[allow(dead_code)]
+fn main_001() {
+    let mut s = String::from("hello");
+    change(&mut s);
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+fn change(some_string: &mut String) {
+    some_string.push_str(", World");
+}
+
+#[allow(dead_code)]
+fn main_002() {
+    let mut s = String::from("hello");
+
+    let r1 = &s;
+    let r2 = &s;
+    println!("{}, and {}", r1, r2);
+    
+    let r3 = &mut s;
+    println!("{}", r3);
+} 
