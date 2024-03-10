cb6faf7a0726d5bad1f86f80a0a678dba156c9e8
createjin <createsejin@gmail.com>
2024-03-10 15:51:25 +0900

    2024-03-10-004

---
+++ src/study/study08.rs
@@ -0,0 +1,9 @@
+#[allow(dead_code)]
+pub fn study001() {
+    #[allow(unused_variables)]
+    let mut v: Vec<i32> = Vec::new();
+    v.push(5); v.push(6); v.push(7); v.push(8);
+    
+    #[allow(unused_variables)]
+    let v2 = vec![1, 2, 3];
+}
