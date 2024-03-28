a9480f57dbd925962e74a519c6c5bee4b522265c
createjin <createsejin@gmail.com>
2024-03-28 12:50:04 +0900

    2024-03-28-002

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use rust_study_001::study;
-
+///home/bae/Projects/rust_study_001/target/debug/rust_study_001
 fn main() {
-    study::study09::_study002();
+    study::study09::_study003();
 }
--- src/study/study09.rs
+++ src/study/study09.rs
@@ -1,3 +1,6 @@
+use core::panic;
+use std::fs::File;
+
 // Error Handling
 pub fn _study001() {
     panic!("crash and burn");
@@ -5,4 +8,28 @@ pub fn _study001() {
 pub fn _study002() {
     let v = vec![1, 2, 3];
     v[99];
+}
+pub fn _study003() {
+    let _greeting_file_result = File::open("hello.txt");
+
+    let _greeting_file = match _greeting_file_result {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
+}
+pub fn _study004() {
+    let _greeting_file_result = File::open("hello.txt");
+
+    let _greeting_file = match _greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            std::io::ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error);
+            }
+        },
+    };
 }
