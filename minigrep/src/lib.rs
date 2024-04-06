82ca5719418e7fbacdc68fa580d19da46aa20f65
createjin <createsejin@gmail.com>
2024-04-06 16:22:21 +0900

    2024-04-06-012 add environment variable feature

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,9 +1,11 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
 pub struct Config {
   pub query: String,
   pub file_path: String,
+  pub ignore_case: bool,
 }
 impl Config {
   #[allow(dead_code)]
@@ -14,9 +16,16 @@ impl Config {
     let query = args[1].clone();
     let file_path = args[2].clone();
 
-    Self { query, file_path }
+    let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+    Self {
+      query,
+      file_path,
+      ignore_case,
+    }
   }
 
+  //@#build
   pub fn build(args: &[String]) -> Result<Self, &'static str> {
     if args.len() < 3 {
       return Err("arguemnts not enough!");
@@ -24,7 +33,13 @@ impl Config {
     let query = args[1].clone();
     let file_path = args[2].clone();
 
-    Ok(Self { query, file_path })
+    let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+    Ok(Self {
+      query,
+      file_path,
+      ignore_case,
+    })
   }
   // pub fn get_query(&self) -> &str {
   //   &self.query
@@ -34,15 +49,23 @@ impl Config {
   // }
 }
 
+//@#run
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
   let contents = fs::read_to_string(config.file_path)?;
 
-  for item in search(&config.query, &contents) {
+  let results = if config.ignore_case {
+    search_case_insensitive(&config.query, &contents)
+  } else {
+    search(&config.query, &contents)
+  };
+
+  for item in results {
     println!("{item}");
   }
   Ok(())
 }
 
+//@#sear.sens
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
   let mut result: Vec<&'a str> = Vec::new();
   for line in contents.lines() {
@@ -53,6 +76,7 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
   result
 }
 
+//@#sear.in
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
   let query = query.to_lowercase();
   let mut result = Vec::new();
