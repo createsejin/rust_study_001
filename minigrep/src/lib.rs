cefdc01828d3a754205aedd8e97ed702f245ac61
createjin <createsejin@gmail.com>
2024-04-06 15:59:18 +0900

    2024-04-06-011 add search_case_insensitive function and passed tests.

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -53,21 +53,48 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
   result
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+  let query = query.to_lowercase();
+  let mut result = Vec::new();
+  for line in contents.lines() {
+    if line.to_lowercase().contains(&query) {
+      result.push(line);
+    }
+  }
+  result
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
 
   #[test]
-  fn one_result() {
+  fn case_sensitive() {
     let query = "duct";
-    let content = "\
+    let contents = "\
 Rust:
 safe, fast, productive.
 Pick three.
-doduct do do what?";
+doduct do do what?
+Duct tape.";
     assert_eq!(
       vec!["safe, fast, productive.", "doduct do do what?"],
-      search(query, content)
+      search(query, contents)
+    );
+  }
+
+  #[test]
+  fn case_insensitive() {
+    let query = "rUsT";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+doduct do do what?
+Trust me.";
+    assert_eq!(
+      vec!["Rust:", "Trust me."],
+      search_case_insensitive(query, contents)
     );
   }
 }
