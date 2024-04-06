81bfe0ff82793035b91e80b1671bf5feb1c5aba7
createjin <createsejin@gmail.com>
2024-04-06 11:52:49 +0900

    2024-04-06-005 Returning a Result Instead of Calling panic!

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,7 +6,8 @@ fn main() {
   let args: Vec<String> = env::args().collect();
   // dbg!(args);
 
-  let config = Config::new(&args);
+  // let config = Config::new(&args);
+  let config = Config::build(&args).unwrap();
 
   println!(r#"Searching for '{}'"#, config.query);
   println!(r#"In file "{}""#, config.file_path);
@@ -22,6 +23,7 @@ struct Config {
   file_path: String,
 }
 impl Config {
+  #[allow(dead_code)]
   fn new(args: &[String]) -> Self {
     if args.len() != 3 {
       panic!("arguemnts number must be 2!");
@@ -30,6 +32,16 @@ impl Config {
     let file_path = args[2].clone();
 
     Self { query, file_path }
+  }
+
+  fn build(args: &[String]) -> Result<Self, &'static str> {
+    if args.len() != 3 {
+      return Err("arguemnts number must be 2!");
+    }
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Ok(Self { query, file_path })
   }
 }
 
