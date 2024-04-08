746b4b7e74ca0e0c05e430841a8978e209248eae
createjin <createsejin@gmail.com>
2024-04-08 15:43:07 +0900

    2024-04-08-002 input parsing complete

--- study001/src/leetcode/leet002.rs
+++ study001/src/leetcode/leet002.rs
@@ -19,8 +19,27 @@ pub fn test001() {
     }
   }
 
-  let students_vec = &parts[0..sandwiche_index];
+  let students_vec = remove_extra_chars(&parts[0..sandwiche_index], students_str);
+  let students_vec = convert_str_vec_to_i32_vec(students_vec);
   println!("{:?}", students_vec);
-  let sandwiche_vec = &parts[sandwiche_index..];
-  println!("{:?}", sandwiche_vec);
+  let sandwiches_vec = remove_extra_chars(&parts[sandwiche_index..], sandwiche_str);
+  let sandwiches_vec = convert_str_vec_to_i32_vec(sandwiches_vec);
+  println!("{:?}", sandwiches_vec);
+}
+
+fn convert_str_vec_to_i32_vec(input: Vec<String>) -> Vec<i32> {
+  let mut output: Vec<i32> = Vec::new();
+  for str in input {
+    output.push(str.as_str().parse().unwrap());
+  }
+  output
+}
+
+fn remove_extra_chars(part_vec: &[&str], extra: &str) -> Vec<String> {
+  let last_num = part_vec.last().unwrap().replace(']', "");
+  let mut part_vec: Vec<String> = part_vec.iter().map(|x| x.to_string()).collect();
+  part_vec[0] = part_vec[0].replace(extra, "");
+  let part_vec_size = part_vec.len();
+  part_vec[part_vec_size - 1] = last_num.to_string();
+  part_vec
 }
