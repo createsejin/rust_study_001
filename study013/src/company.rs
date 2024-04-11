cef3958187700227b392119ef5d5a6005cdb1acc
createjin <createsejin@gmail.com>
2024-04-11 18:15:39 +0900

    Closure Type Inference and Annotation

--- study013/src/company.rs
+++ study013/src/company.rs
@@ -1,3 +1,5 @@
+use std::{thread, time::Duration};
+
 #[derive(Debug, PartialEq, Copy, Clone)]
 pub enum ShirtColor {
   Red,
@@ -47,4 +49,25 @@ pub fn user_helper(user_pref: &Option<ShirtColor>, store: &Inventory) {
     "The user with preference {} gets {:?}",
     user_pref_color, giveaway
   );
+}
+
+#[allow(dead_code)]
+fn add_one_v1(x: u32) -> u32 {
+  x + 1
+}
+pub fn _study001() {
+  let _expensive_closure = |num: u32| -> u32 {
+    println!("calculating slowly...");
+    thread::sleep(Duration::from_secs(2));
+    num
+  };
+  let _add_one_v2 = |x: u32| -> u32 { x + 1 };
+  let _add_one_v3 = |x: u32| x + 1;
+}
+
+pub fn _study002() {
+  let example_closure = |x| x;
+
+  let s = example_closure(String::from("hello"));
+  let n = example_closure(5);
 }
--- study013/src/main.rs
+++ study013/src/main.rs
@@ -1,14 +1,5 @@
 mod company;
-use company::ShirtColor;
 
 fn main() {
-  let store = company::Inventory {
-    shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
-  };
-
-  let user_pref1 = Some(ShirtColor::Red);
-  company::user_helper(&user_pref1, &store);
-
-  let user_pref2 = None;
-  company::user_helper(&user_pref2, &store);
+  company::_study002();
 }
