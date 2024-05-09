01ac86169d27ddd8c5feb72613c2e0c588c138a3
createjin <createsejin@gmail.com>
2024-05-10 04:16:21 +0900

    Listing 15-5: Definition of List that uses Box<T> in order to have a known size

--- study014/study/study_lib/src/study15.rs
+++ study014/study/study_lib/src/study15.rs
@@ -2,13 +2,25 @@ pub fn study001() {
   println!("call study001");
 }
 
+#[allow(dead_code)]
+enum List<T> {
+  Cons(T, Box<List<T>>),
+  Nil,
+}
+
 #[cfg(test)]
 mod tests_study15 {
-  // use super::*;
+  use super::*;
 
   #[test]
   fn study002() {
     let b = Box::new(5);
     println!("b = {}", b);
+  }
+
+  #[test]
+  fn study003() {
+    use List::{Cons, Nil};
+    let _list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
   }
 }
